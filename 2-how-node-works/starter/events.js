3bc90dce76d0c3165124990a99fe4b95de837e10
haliluysal <haliluysal@gmail.com>
2023-12-25 12:12:26 -0500

    server listeners

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,6 +1,13 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
@@ -11,3 +18,24 @@ myEmitter.on("newSale", () => {
 });
 
 myEmitter.emit("newSale");
+
+/////////////////////////////////////////////
+
+const server = http.createServer();
+
+server.on("request", (req, resp) => {
+  console.log("Request received");
+  resp.end("Request received");
+});
+
+server.on("request", (req, resp) => {
+  console.log("Another Request received 💵");
+});
+
+server.on("close", (req, resp) => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests");
+});
