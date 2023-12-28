4fc7825174379131a45a1ec1039d80de42a9c7e9
haliluysal <haliluysal@gmail.com>
2023-12-27 21:04:26 -0500

    async callback hell started

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, resp) => {
+      if (err) return console.log(err.message);
+      console.log(resp.body.message);
+      fs.writeFile("dog-img.txt", resp.body.message, (err) => {
+        if (err) return console.log(err.message);
+        if (err) console.log("saved file");
+      });
+    });
+});
