58d6f50b9f36d39808b6e82b498e776fd4b70076
haliluysal <haliluysal@gmail.com>
2024-01-05 06:15:51 -0500

    get and patch requests for specific id

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
-
+app.use(express.json()); // middleware, between request and response
 // app.get('/', (req, res) => {
 //   res.status(200).json({
 //     message: 'Hello from the server side!',
@@ -17,14 +17,71 @@ const app = express();
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
-app.get('/api/v1/tours', (req, resp) => {
-  resp.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: {
-      tours: tours,
-    },
-  });
+// app.get('/api/v1/tours', (req, resp) => {
+//   resp.status(200).json({
+//     status: 'success',
+//     results: tours.length,
+//     data: {
+//       tours: tours,
+//     },
+//   });
+// });
+
+app.get('/api/v1/tours/:id?', (req, resp) => {
+  let tour = tours;
+  if (req.params.id) {
+    const id = req.params.id * 1;
+    tour = tours.find((el) => el.id === id);
+  }
+  if (tour) {
+    resp.status(200).json({
+      status: 'success',
+      data: { tours: tour },
+    });
+  } else {
+    resp.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+});
+
+app.post('/api/v1/tours', (req, resp) => {
+  //   console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      resp.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+app.patch('/api/v1/tours/:id', (req, resp) => {
+  let tour = tours;
+  if (req.params.id) {
+    const id = req.params.id * 1;
+    tour = tours.find((el) => el.id === id);
+  }
+  if (tour) {
+    resp.status(200).json({
+      status: 'success',
+      data: { tour: '<updated tour>' },
+    });
+  } else {
+    resp.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
 });
 
 const port = 3000;
