dd8760a5f5088d3fe0956a1f51c359c9eaa506ac
haliluysal <haliluysal@gmail.com>
2024-01-10 05:52:27 -0500

    get tour id middle ware

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -4,6 +4,11 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
 
+router.param('id', (req, resp, next, val) => {
+  console.log(`Tour id is ${val}`); // value is the parameter
+  next();
+});
+
 router
   .route('/')
   .get(tourController.getAllTours)
@@ -12,6 +17,6 @@ router
   .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour);
-router.route('/:id?').delete(tourController.deleteTour);
+router.route('/:id').delete(tourController.deleteTour);
 
 module.exports = router;
