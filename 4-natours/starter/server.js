dbc7937cc9b6a66371cf28017a9be0b06a518154
haliluysal <haliluysal@gmail.com>
2024-01-16 21:00:16 -0500

    removing config

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './config2.env' });
 
 const db = process.env.DATABASE.replace(
   '<PASSWORD>',
