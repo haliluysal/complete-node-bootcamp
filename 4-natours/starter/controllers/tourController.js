3d7073a6c5f999ea90f0855b2faa2b8ed0df0e61
haliluysal <haliluysal@gmail.com>
2024-01-31 21:02:16 -0500

    filtering etc.

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -3,7 +3,41 @@ const Tour = require('../models/tourModel');
 // ROUTE HANDLES
 exports.getAllTours = async (req, resp) => {
   try {
-    const tours = await Tour.find();
+    // build query
+    // 1A) filtering
+    const queryObj = { ...req.query }; // deep copy
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2B) advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    const tours = await query;
+
     resp.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
