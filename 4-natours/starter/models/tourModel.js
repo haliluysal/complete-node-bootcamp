27ccc6d066dd67f0be201113cd8658a4c0733972
haliluysal <haliluysal@gmail.com>
2024-02-19 12:11:16 -0500

    aggregation middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -95,9 +95,19 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took: ${Date.now() - this.start} ms`);
-  console.log(docs);
+  // console.log(docs);
   next();
 });
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } },
+  });
+  console.log(this.pipeline());
+  next();
+});
+
 // tourSchema.post('save', function (doc, next) {
 //   console.log(doc);
 //   next();
